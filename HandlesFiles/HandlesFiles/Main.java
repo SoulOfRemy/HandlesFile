6c0a4861a9e71f53b6ecf2f76244de92170f9a82
Remy <137978727+SoulOfRemy@users.noreply.github.com>
2024-02-28 10:26:50 +0700

    Add files via upload

---
+++ HandlesFiles/HandlesFiles/Main.java
@@ -0,0 +1,10 @@
+
+import Controller.FileHandler;
+
+public class Main {
+    public static void main(String[] args) {
+        new FileHandler().run();
+        // src\PersonInfo.txt
+        // src\copy.txt
+    }
+}
